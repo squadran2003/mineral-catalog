cce576888d41b9b3f282ca77f3d24fe48b790d45
Andreas Cormack <cormackandy@hotmail.com>
2017-05-14 11:57:12 +0100

    database settings to deploy

--- mineralcatalog/mineralcatalog/deploy_settings/__init__.py
+++ mineralcatalog/mineralcatalog/deploy_settings/__init__.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from ..settings import *
 
 DEBUG = False
@@ -11,3 +12,6 @@ ALLOWED_HOSTS = [
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
 STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
