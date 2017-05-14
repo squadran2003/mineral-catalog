74b53ac11a1cf15bfb9fbf85402f6d496e03ad0f
Andreas Cormack <cormackandy@hotmail.com>
2017-05-14 10:39:33 +0100

    add deploy settings

--- mineralcatalog/mineralcatalog/settings.py
+++ mineralcatalog/mineralcatalog/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+def get_env_variable(var_name):
+    """get the enviroment varibale or return an exception"""
+    try:
+        os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the {} env variable".format(var_name)
+        if DEBUG:
+            warnings.warn(error_msg)
+        else:
+            raise ImproperlyConfigured(error_msg)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,9 +37,8 @@ SECRET_KEY = '5yovvx%lbe6w8k7a@u!7^(rc!az#%#=#3_9-94z(v1#2ihi$ql'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'squadran2003.pythonanywhere.com',
-]
+ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -120,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 
