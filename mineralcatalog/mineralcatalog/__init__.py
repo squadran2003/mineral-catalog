fe3008534efa4f1cd362575bee2d76d080360749
Andreas Cormack <cormackandy@hotmail.com>
2017-05-14 11:05:28 +0100

    added deploy settings

--- mineralcatalog/mineralcatalog/__init__.py
+++ mineralcatalog/mineralcatalog/__init__.py
@@ -1,13 +1,1 @@
-from .settings import *
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '.pythonanywhere.com',
-]
-
-SECRET_KEY = get_env_variable("SECRET_KEY")
-
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
